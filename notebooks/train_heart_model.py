b33178400da764b4579c1bffd084aa8cc1ff9b6a
Armaan Anas <armaananas03@gmail.com>
2026-07-13 10:55:43 +0530

    Add diabetes prediction and dashboard enhancements

---
+++ notebooks/train_heart_model.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("datasets/heart.csv")
+
+X = df.drop("target", axis=1)
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(
+    y_test,
+    predictions
+)
+
+print("Accuracy:", round(accuracy, 2))
+
+joblib.dump(
+    model,
+    "trained_models/heart_disease_model.pkl"
+)
+
+print("Heart Disease Model Saved Successfully!")
